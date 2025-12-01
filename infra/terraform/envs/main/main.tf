55f707a8f5c5593c4bf3efa424b7174f0c1063ae
Kayla <kaylavd99@gmail.com>
2025-11-30 19:53:55 -0500

    Fixed cors type for post to POST

--- infra/terraform/envs/main/main.tf
+++ infra/terraform/envs/main/main.tf
@@ -180,7 +180,7 @@ resource "aws_lambda_function_url" "generate" {
 
     cors {
         allow_origins = ["*"]
-        allow_methods = ["post"]
+        allow_methods = ["POST"]
         allow_headers = ["content-type"]
     }
 
